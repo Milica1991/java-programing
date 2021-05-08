f78be0cf85b5596d826b19c56c70fcc2794a4b0e
Milica <djoric.milica91@yahoo.com>
2021-05-08 17:11:46 -0400

    Update!

--- src/day43_list_custom_classes/Person.java
+++ src/day43_list_custom_classes/Person.java
@@ -1,8 +1,14 @@
 package day43_list_custom_classes;
 
 public class Person {
+    //Data -> variable
     String firstName;
     int age;
+    char gender;
+    //behaviour - method
+    public void speak() {
+        System.out.println("person is speaking");
+    }
 }
 class People {
     public static void main(String[] args) {
@@ -10,9 +16,25 @@ class People {
         Person person1 = new Person();
         person1.firstName = "Milica";
         person1.age = 30;
+        person1.gender = 'F';
+        person1.speak();
 
         System.out.println(person1.firstName);
         System.out.println(person1.age);
+        System.out.println(person1.gender);
+
+        Person person2 = new Person();
+        person2.firstName = "Mike";
+        person2.age = 12;
+        person2.gender = 'M';
+        person2.speak();
+
+
+        System.out.println(person2.firstName);
+        System.out.println(person2.age);
+        System.out.println(person2.gender);
+
+        System.out.println("person1.firstname = " + person1.firstName);
 
     }
 }
