380252d3ba9c059763328d3941f24a0977ed67f7
Milica <djoric.milica91@yahoo.com>
2021-05-16 15:39:49 -0400

    Update!

---
+++ src/Milica/gng.java
@@ -0,0 +1,18 @@
+package Milica;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class gng {
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("during","storm","rain",
+                "fell","season","cloudy","safe"));
+        ArrayList<Integer> lengths = new ArrayList<>();
+        for (String word: words){
+            lengths.add(word.length());
+        }
+        System.out.println(lengths);
+
+
+    }
+}
