8ced22cb749b73314b1a995a756dabaafa566b8b
Milica <djoric.milica91@yahoo.com>
2021-05-28 07:25:44 -0400

    added student class

---
+++ src/day47_constructors/Student.java
@@ -0,0 +1,24 @@
+package day47_constructors;
+
+public class Student {
+    private String name;
+    private String age;
+    //No args constructor'
+    public Student() {
+        System.out.println("No-Args constructor");
+    }
+    // consrtuctor with 1 param: String name
+    public Student (String name) {
+        System.out.println("name param constructor | name = " + name);
+    }
+    //constructor with age
+    public Student(int age) {
+        System.out.println("age param constructor | age = " + age);
+    }
+    //constructor with name and age
+    public Student(String name, int age){
+        System.out.println("name&age param constructor | " + name +"-"+age);
+    }
+
+}
+
