b3e5c2fa520ad82b80b6b828713078ed58acdbaa
Milica <djoric.milica91@yahoo.com>
2021-06-05 11:40:07 -0400

    Car

---
+++ src/day49_static/Locators.java
@@ -0,0 +1,7 @@
+package day49_static;
+
+public class Locators {
+    public static String userName = "//*[@id='prepedndInput']";
+    public static String password = "//*[@id='prepedndInput2']";
+    public static String loginBtn = "//*[@id='login']";
+}
--- src/day49_static/StaticVeriablesTest.java
+++ src/day49_static/StaticVeriablesTest.java
@@ -6,6 +6,8 @@ public class StaticVeriablesTest {
         System.out.println(StaticVariables.userName);
         System.out.println(StaticVariables.age);
         System.out.println(StaticVariables.price);
-
+        System.out.println("driver.findElement(By.xpath("+Locators.userName+").sendKeys("+ StaticVariables.userName);
+        System.out.println(Locators.password);
+        System.out.println(Integer.MAX_VALUE);
     }
 }
