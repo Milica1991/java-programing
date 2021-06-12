97db31d51207951baa2ccec3bc2e41a7b55b23be
Milica <djoric.milica91@yahoo.com>
2021-06-12 17:02:24 -0400

    super() -parent class constructor

---
+++ src/day52_inheritance/discordusers/Admin.java
@@ -0,0 +1,24 @@
+package day52_inheritance.discordusers;
+
+public class Admin extends User{
+
+    public Admin() {
+        super(); //call super class? User class no- args constructors
+        System.out.println("Admin class constructor");
+    }
+
+    public Admin(String role, String name, int id) {
+        super(role, name, id);
+        System.out.println("Admin class 2 args constructor");
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "role='" + getRole() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", id=" + getId() +
+                '}';
+    }
+
+}
